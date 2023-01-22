dc6eb0d53c34d9efc23fffb2f8b30d4ccb357e50
shaikh shoaib <shaikhshoaib8879@gmail.com>
2023-01-23 00:21:27 +0530

    hooks completed

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,14 @@
 
 import { useState} from "react";
 
-
+const ANIMALS = ["birds","cat","dog","rabbot"];
 
 const SearchParams = () => {
 
     const [location, setLocation] = useState("");
+    const [animal , setAnimal] = useState("");
+    const [breed, setBreed] = useState("");
+    const breeds= ["shepher","persian","french","bulldog","cobra"];
     return (
         <div className="search-params">
             <form>
@@ -16,6 +19,37 @@ const SearchParams = () => {
                         value={location} 
                         placeholder="Location"
                     />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={(e) => {
+                            setAnimal(e.target.value);
+                        }}
+                    >
+                            <option />
+                            {ANIMALS.map((animal) => (
+                                <option key={animal}>{animal}</option>
+                            ))}
+                        </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        id="breed"
+                        disabled={breeds.length == 0}
+                        value={breed}
+                        onChange={(e) => {
+                            setBreed(e.target.value);
+                        }}
+                    >
+                            <option />
+                            {breeds.map((breed) => (
+                                <option key={breed}>{breed}</option>
+                            ))}
+                        </select>
                 </label>
             <button>Submit</button>
             </form>
